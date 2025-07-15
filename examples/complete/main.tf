de6650231b974b24d2a91c40c4ad3abdb3e0c942
Igor Rodionov <goruha@gmail.com>
2025-07-16 00:11:40 +0200

    Fix fomatting

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -2,37 +2,52 @@ module "example" {
   source  = "../.."
   context = module.this.context
 
-  description                             = var.description
-  visibility                              = var.visibility
-  homepage_url                            = var.homepage_url
-  archived                                = var.archived
-  has_issues                              = var.has_issues
-  has_projects                            = var.has_projects
-  has_discussions                         = var.has_discussions
-  has_wiki                                = var.has_wiki
-  archive_on_destroy                      = var.archive_on_destroy
-  autolink_references                     = var.autolink_references
-  default_branch                          = var.default_branch
-  web_commit_signoff_required             = var.web_commit_signoff_required
-  topics                                  = var.topics
-  license_template                        = var.license_template
-  gitignore_template                      = var.gitignore_template
-  auto_init                               = var.auto_init
+  name = module.this.id
+
+  description = var.description
+  visibility  = var.visibility
+
+  homepage_url = var.homepage_url
+  topics       = var.topics
+
+  archived           = var.archived
+  archive_on_destroy = var.archive_on_destroy
+
+  is_template = var.is_template
+
+  has_discussions = var.has_discussions
+  has_downloads   = var.has_downloads
+  has_issues      = var.has_issues
+  has_projects    = var.has_projects
+  has_wiki        = var.has_wiki
+
+  allow_squash_merge = var.allow_squash_merge
+  allow_merge_commit = var.allow_merge_commit
+  allow_rebase_merge = var.allow_rebase_merge
+
+  squash_merge_commit_title   = var.squash_merge_commit_title
+  squash_merge_commit_message = var.squash_merge_commit_message
+
+  allow_auto_merge = var.allow_auto_merge
+
+  merge_commit_title   = var.merge_commit_title
+  merge_commit_message = var.merge_commit_message
+
+  allow_update_branch    = var.allow_update_branch
+  delete_branch_on_merge = var.delete_branch_on_merge
+
+  auto_init          = var.auto_init
+  gitignore_template = var.gitignore_template
+  license_template   = var.license_template
+
+  web_commit_signoff_required = var.web_commit_signoff_required
+
   ignore_vulnerability_alerts_during_read = var.ignore_vulnerability_alerts_during_read
-  enable_vulnerability_alerts             = var.enable_vulnerability_alerts
-  allow_update_branch                     = var.allow_update_branch
-  security_and_analysis                   = var.security_and_analysis
-  allow_squash_merge                      = var.allow_squash_merge
-  squash_merge_commit_title               = var.squash_merge_commit_title
-  squash_merge_commit_message             = var.squash_merge_commit_message
-  allow_merge_commit                      = var.allow_merge_commit
-  merge_commit_title                      = var.merge_commit_title
-  merge_commit_message                    = var.merge_commit_message
-  allow_rebase_merge                      = var.allow_rebase_merge
-  delete_branch_on_merge                  = var.delete_branch_on_merge
-  is_template                             = var.is_template
-  has_downloads                           = var.has_downloads
-  allow_auto_merge                        = var.allow_auto_merge
+
+  autolink_references         = var.autolink_references
+  default_branch              = var.default_branch
+  enable_vulnerability_alerts = var.enable_vulnerability_alerts
+  security_and_analysis       = var.security_and_analysis
 
   custom_properties = var.custom_properties
   environments      = var.environments
